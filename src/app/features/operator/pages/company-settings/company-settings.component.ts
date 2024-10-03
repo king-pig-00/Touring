f9da8c7ee3ca0ee73754d235cf6de87b9e76e26d
John Pate <john.pate.programmer@gmail.com>
2024-10-03 14:47:30 +0800

    #7 - Create files for page structure

---
+++ src/app/features/operator/pages/company-settings/company-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-settings.component.html',
+  styleUrl: './company-settings.component.scss'
+})
+export class CompanySettingsComponent {
+
+}
