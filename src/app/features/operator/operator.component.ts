bc340bbf91cb90726d9aa59145d0755f0cbe57ac
John Pate <john.pate.programmer@gmail.com>
2024-10-03 14:58:27 +0800

    #7 - Setup routes for page structure

--- src/app/features/operator/operator.component.ts
+++ src/app/features/operator/operator.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-operator',
-  standalone: true,
-  imports: [],
-  templateUrl: './operator.component.html',
-  styleUrl: './operator.component.scss'
+    selector: 'app-operator',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './operator.component.html',
+    styleUrl: './operator.component.scss',
 })
-export class OperatorComponent {
-
-}
+export class OperatorComponent {}
--- src/app/features/operator/pages/company-settings/company-settings.component.ts
+++ src/app/features/operator/pages/company-settings/company-settings.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-company-settings',
-  standalone: true,
-  imports: [],
-  templateUrl: './company-settings.component.html',
-  styleUrl: './company-settings.component.scss'
+    selector: 'app-company-settings',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './company-settings.component.html',
+    styleUrl: './company-settings.component.scss',
 })
-export class CompanySettingsComponent {
-
-}
+export class CompanySettingsComponent {}
